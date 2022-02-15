5647a2a5024b7cdc18a49a9c8685c9236ed02e00
Alejandro <amnaredo@gmail.com>
2022-02-15 23:34:26 +0100

    add filter search options

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use crate::EntryType::*;
 use clap::{App, Arg};
 use regex::Regex;
 use std::error::Error;
+use walkdir::WalkDir;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -78,6 +79,7 @@ pub fn get_args() -> MyResult<Config> {
     
     
     Ok(Config {
+
         dirs: matches.values_of_lossy("dirs").unwrap(),
         names: if names.is_empty() { None } else { Some(names) },
         entry_types
@@ -85,7 +87,49 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+
+    let mut show_dirs = true;
+    let mut show_files = true;
+    let mut show_links = true;
+
+    if config.entry_types.is_some() {
+        let types_to_show = config.entry_types.unwrap();
+
+        show_dirs = types_to_show.contains(&EntryType::Dir);
+        show_files = types_to_show.contains(&EntryType::File);
+        show_links = types_to_show.contains(&EntryType::Link);
+    }
+
+    for dirname in &config.dirs {
+        for entry in WalkDir::new(dirname) {
+            // println!("{}", entry?.path().display());
+            match entry {
+                Err(err) => eprintln!("{}: {}", dirname, err),
+                Ok(dir_entry) => {
+
+                    let filetype = dir_entry.path().metadata()?.file_type();
+                    let type_match = 
+                        filetype.is_dir() && show_dirs || 
+                        filetype.is_file() && show_files || 
+                        filetype.is_symlink() && show_links;
+
+                    let mut regex_match = true;
+                    if let Some(regexps) = &config.names {
+                        regex_match = regexps
+                            .iter()
+                            .any(|regex| {
+                                 regex.is_match(&dir_entry.file_name().to_string_lossy())
+                            }
+                        );
+                    }
+
+                    if type_match && regex_match {
+                        println!("{}", dir_entry.path().display());
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
 
